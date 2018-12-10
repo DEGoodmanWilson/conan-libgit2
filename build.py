60748c7b8e9f2dc33b14c307419480497a46e3d7
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-12-10 15:25:02 +0100

    Indentation errors, FU Python

--- build.py
+++ build.py
@@ -5,10 +5,10 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-	name = get_name_from_recipe()
+    name = get_name_from_recipe()
     username, channel, version, login_username = get_conan_vars()
     reference = "{0}/{1}".format(name, version)
-	upload = get_conan_upload(username)
+    upload = get_conan_upload(username)
     bincrafters = "https://api.bintray.com/conan/bincrafters/public-conan"
 
     builder = build_template_default.get_builder(remotes=[upload, bincrafters])
